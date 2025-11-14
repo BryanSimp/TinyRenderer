c05094a224172702fbfdc8880bda5938b9a0601a
Bryan Simpson <simpsonbm04@outlook.com>
2025-11-14 16:38:17 -0600

    Added line drawing and optimized it
    For this push I learned about Bresenham's line drawing algorithm which efficiently draws a line by figures out the direction of the line to prevent negative steps by  making it left to right and if the line is steep or not and if so transposes it

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -1,35 +1,52 @@
-#include <cmath>
+﻿#include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include "tgaimage.h"
 
-constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
-constexpr TGAColor green = { 0, 255,   0, 255 };
-constexpr TGAColor red = { 0,   0, 255, 255 };
-constexpr TGAColor blue = { 255, 128,  64, 255 };
-constexpr TGAColor yellow = { 0, 200, 255, 255 };
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep) { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx) { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int ierror = 0;
+    for (int x = ax; x <= bx; x++) {
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+        ierror += 2 * std::abs(by - ay);
+        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
+        ierror -= 2 * (bx - ax) * (ierror > bx - ax);
+    }
+}
 
 int main(int argc, char** argv) {
-    constexpr int width = 64;
+    constexpr int width  = 64;
     constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
-    
-    //original code
-    int ax = 7, ay = 3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
 
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
-    
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    /*
-    Fun Testing 
-    Simple Line
-    for (int x = 0; x < 64; x++) {
-        framebuffer.set(x, x, white);
+    for (int i = 0; i < (1 << 18); i++) { // reduce iterations for faster testing
+        int ax = rand() % width, ay = rand() % height;
+        int bx = rand() % width, by = rand() % height;
+
+        std::uint8_t b = static_cast<std::uint8_t>(rand() % 256);
+        std::uint8_t g = static_cast<std::uint8_t>(rand() % 256);
+        std::uint8_t r = static_cast<std::uint8_t>(rand() % 256);
+
+        // BGRA ordering; bytespp must match image’s bpp
+        TGAColor color = { {b, g, r, 255}, 3 };
+
+        line(ax, ay, bx, by, framebuffer, color);
     }
-    */
-    
+
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
