67aaa78e2b1c4fa260194618bbf3749158a2c70e
Bryan Simpson <simpsonbm04@outlook.com>
2025-11-15 15:37:01 -0600

    Model Rendering
    The program can now render models using the model in the path obj/model.obj

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -1,7 +1,17 @@
 ﻿#include <cmath>
-#include <cstdlib>
-#include <ctime>
+#include <tuple>
+#include "geometry.h"
+#include "model.h"
 #include "tgaimage.h"
+
+constexpr int width = 800;
+constexpr int height = 800;
+
+constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
+constexpr TGAColor green = { 0, 255,   0, 255 };
+constexpr TGAColor red = { 0,   0, 255, 255 };
+constexpr TGAColor blue = { 255, 128,  64, 255 };
+constexpr TGAColor yellow = { 0, 200, 255, 255 };
 
 void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
     bool steep = std::abs(ax - bx) < std::abs(ay - by);
@@ -21,32 +31,41 @@ void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
         else
             framebuffer.set(x, y, color);
         ierror += 2 * std::abs(by - ay);
-        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
-        ierror -= 2 * (bx - ax) * (ierror > bx - ax);
+        if (ierror > bx - ax) {
+            y += by > ay ? 1 : -1;
+            ierror -= 2 * (bx - ax);
+        }
     }
 }
 
+std::tuple<int, int> project(vec3 v) { // First of all, (x,y) is an orthogonal projection of the vector (x,y,z).
+    return { (v.x + 1.) * width / 2,   // Second, since the input models are scaled to have fit in the [-1,1]^3 world coordinates,
+             (v.y + 1.) * height / 2 }; // we want to shift the vector (x,y) and then scale it to span the entire screen.
+}
+
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
-    TGAImage framebuffer(width, height, TGAImage::RGB);
-
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-    for (int i = 0; i < (1 << 18); i++) { // reduce iterations for faster testing
-        int ax = rand() % width, ay = rand() % height;
-        int bx = rand() % width, by = rand() % height;
-
-        std::uint8_t b = static_cast<std::uint8_t>(rand() % 256);
-        std::uint8_t g = static_cast<std::uint8_t>(rand() % 256);
-        std::uint8_t r = static_cast<std::uint8_t>(rand() % 256);
-
-        // BGRA ordering; bytespp must match image’s bpp
-        TGAColor color = { {b, g, r, 255}, 3 };
-
-        line(ax, ay, bx, by, framebuffer, color);
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1;
     }
 
+    Model model(argv[1]);
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    for (int i = 0; i < model.nfaces(); i++) { // iterate through all triangles
+        auto [ax, ay] = project(model.vert(i, 0));
+        auto [bx, by] = project(model.vert(i, 1));
+        auto [cx, cy] = project(model.vert(i, 2));
+        line(ax, ay, bx, by, framebuffer, red);
+        line(bx, by, cx, cy, framebuffer, red);
+        line(cx, cy, ax, ay, framebuffer, red);
+    }
+
+    for (int i = 0; i < model.nverts(); i++) { // iterate through all vertices
+        vec3 v = model.vert(i);            // get i-th vertex
+        auto [x, y] = project(v);          // project it to the screen
+        framebuffer.set(x, y, white);
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
